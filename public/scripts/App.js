9d687ce25d1ac7f412d2eca29cdd25a7e5c7370f
alestor123 <alestoraldous@gmail.com>
2020-12-26 16:35:54 +0530

    added time

--- public/scripts/App.js
+++ public/scripts/App.js
@@ -3,3 +3,7 @@ monthNames = ["January", "February", "March", "April", "May", "June",
 "July", "August", "September", "October", "November", "December"],
 time = document.getElementById('time'),
 year = document.getElementById('year');
+setInterval(()=> {
+var today = new Date()
+time.innerHTML =  monthNames[today.getMonth()] + ' ' + days[today.getDay()] + '  ' + today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
+}, 500);

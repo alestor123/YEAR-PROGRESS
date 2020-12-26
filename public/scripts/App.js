a9d1925aca9c013219443f622d25fd6db3dc3494
alestor123 <alestoraldous@gmail.com>
2020-12-26 16:12:58 +0530

    added dom

---
+++ public/scripts/App.js
@@ -0,0 +1,5 @@
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+monthNames = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"],
+time = document.getElementById('time'),
+year = document.getElementById('year');
